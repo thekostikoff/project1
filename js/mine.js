fcf91ab4d75bf9bc15d8f93b66e8d9d0495da172
thekostikoff <kostikoff_terrik@mail.ru>
2022-09-19 15:19:17 +0300

    featture/JS:JS one

--- js/mine.js
+++ js/mine.js
@@ -1,53 +1,38 @@
-function showText() {
-	var dots = document.getElementById('dots');
-	var more = document.getElementById('more');
-	var spanclick = document.getElementById('spanclick');
-
-	if (dots.style === "none") {
-		dots.style.display = "inline";
-		spanclick.innerHTML = "Читать полностью";
-		more.style.display = "none";
-	} else {
-		{
-			dots.style.display = "none";
-			spanclick.innerHTML = "Скрыть";
-			more.style.display = "inline";
-		}
+function Spoiler() {
+	var ele = document.getElementById("more");
+	var text = document.getElementById("spanclick");
+	if (ele.style.display == "block") {
+		ele.style.display = "none";
+		text.innerHTML = "Читать полностью";
+	}
+	else {
+		ele.style.display = "block";
+		text.innerHTML = "Скрыть";
 	}
 }
 
-function showTexttwo() {
-	var dotstwo = document.getElementById('dotstwo');
-	var moretwo = document.getElementById('moretwo');
-	var spanclicktwo = document.getElementById('spanclicktwo');
-
-	if (dotstwo.style === "none") {
-		dotstwo.style.display = "inline";
-		spanclicktwo.innerHTML = "Читать полностью";
-		moretwo.style.display = "none";
-	} else {
-		{
-			dotstwo.style.display = "none";
-			spanclicktwo.innerHTML = "Скрыть";
-			moretwo.style.display = "inline";
-		}
+function Spoilertwo() {
+	var ele = document.getElementById("moretwo");
+	var text = document.getElementById("spanclick-two");
+	if (ele.style.display == "block") {
+		ele.style.display = "none";
+		text.innerHTML = "Читать полностью";
 	}
-}
-function showTextthree() {
-	var dotsthree = document.getElementById('dotsthree');
-	var morethree = document.getElementById('morethree');
-	var spanclickthree = document.getElementById('spanclickthree');
-
-	if (dotsthree.style === "none") {
-		dotsthree.style.display = "inline";
-		spanclickthree.innerHTML = "Читать полностью";
-		morethree.style.display = "none";
-	} else {
-		{
-			dotsthree.style.display = "none";
-			spanclickthree.innerHTML = " Скрыть";
-			morethree.style.display = "inline";
-		}
+	else {
+		ele.style.display = "block";
+		text.innerHTML = "Скрыть";
 	}
 }
 
+function Spoilerth() {
+	var ele = document.getElementById("morethree");
+	var text = document.getElementById("spanclick-three");
+	if (ele.style.display == "block") {
+		ele.style.display = "none";
+		text.innerHTML = "Читать полностью";
+	}
+	else {
+		ele.style.display = "block";
+		text.innerHTML = "Скрыть";
+	}
+}
