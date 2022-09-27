8d2c552b9a6006864be82fcc18da16d96c3ac590
thekostikoff <kostikoff_terrik@mail.ru>
2022-09-27 16:42:55 +0300

    feature/JS:4 part

--- js/service.js
+++ js/service.js
@@ -21,15 +21,11 @@ document.querySelector('.img__right').addEventListener('click', function () {
 	carscars.style.left = -offset + 'px';
 });
 
-function first() {
-	document.getElementById("second_hide").classList.toggle('hidden');
-	document.getElementById("first").classList.toggle('hidden');
-}
 
 function showimg(carsbut) {
 	switch (carsbut) {
 		case "gaz":
-			offset = offset + 1029;
+			offset = offset - 1029;
 			carscars.style.left = offset + 'px';
 			break;
 		case "cabl":
@@ -37,10 +33,18 @@ function showimg(carsbut) {
 			carscars.style.left = offset;
 			break;
 		case "truck":
-			offset = offset - 1029;
-			carscars.style.left = offset - 'px';
+			offset = offset - 2058;
+			carscars.style.left = offset + 'px';
 			break;
 	}
 }
+document.getElementById('gaz').addEventListener('click', () => showimg('gaz'));
+document.getElementById('cabl').addEventListener('click', () => showimg('cabl'));
+document.getElementById('truck').addEventListener('click', () => showimg('truck'));
 
-document.getElementbyId('gaz').addEventListener('click', function showimg(cabl));
+function first() {
+	document.getElementById("second_hide").classList.toggle('hidden');
+	document.getElementById("first").classList.toggle('hidden');
+}
+document.getElementById('serv').addEventListener('click', () => first());
+document.getElementById('first').addEventListener('click', () => first());
