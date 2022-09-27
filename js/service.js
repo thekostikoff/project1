abc3ec5ce7f1ee773fdf937fef82b5974d2f5ac3
thekostikoff <kostikoff_terrik@mail.ru>
2022-09-27 12:31:49 +0300

    feature/JS:3 part

--- js/service.js
+++ js/service.js
@@ -1,5 +1,9 @@
 let offset = 0;
 const carscars = document.querySelector('.cars__cars');
+const carsbut = document.querySelectorAll('.cars__but');
+// const gaz = document.querySelectorAll('gaz');
+// const cabl = document.querySelectorAll('cabl');
+// const truck = document.querySelectorAll('truck');
 
 document.querySelector('.img__left').addEventListener('click', function () {
 	offset = offset + 1029;
@@ -22,3 +26,21 @@ function first() {
 	document.getElementById("first").classList.toggle('hidden');
 }
 
+function showimg(carsbut) {
+	switch (carsbut) {
+		case "gaz":
+			offset = offset + 1029;
+			carscars.style.left = offset + 'px';
+			break;
+		case "cabl":
+			offset = 0;
+			carscars.style.left = offset;
+			break;
+		case "truck":
+			offset = offset - 1029;
+			carscars.style.left = offset - 'px';
+			break;
+	}
+}
+
+document.getElementbyId('gaz').addEventListener('click', function showimg(cabl));
